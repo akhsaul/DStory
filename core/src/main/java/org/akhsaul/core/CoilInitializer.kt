7d95e0e1e9a44e5bd9aa2e91dd5075acff20d920
Akhsaul <ikhsanmaulana384@gmail.com>
2025-05-21 17:56:40 +0700

    feat: Add CoilInitializer for image loading
    This commit introduces `CoilInitializer` to set up Coil for image loading.

--- core/src/main/java/org/akhsaul/core/CoilInitializer.kt
+++ core/src/main/java/org/akhsaul/core/CoilInitializer.kt
@@ -1,4 +1,35 @@
 package org.akhsaul.core
 
-class CoilInitializer {
+import android.content.Context
+import androidx.startup.Initializer
+import coil3.ImageLoader
+import coil3.SingletonImageLoader
+import coil3.disk.DiskCache
+import coil3.disk.directory
+import coil3.memory.MemoryCache
+import coil3.request.crossfade
+import coil3.svg.SvgDecoder
+import java.util.Collections
+
+@Suppress("unused")
+class CoilInitializer : Initializer<ImageLoader> {
+    override fun create(context: Context): ImageLoader {
+        val imageLoader = ImageLoader.Builder(context)
+            .crossfade(true)
+            .components {
+                add(SvgDecoder.Factory())
+            }
+            .memoryCache(MemoryCache.Builder().maxSizePercent(context, 0.25).build())
+            .diskCache(
+                DiskCache.Builder()
+                    .directory(context.cacheDir.resolve("image_cache"))
+                    .maxSizePercent(0.25)
+                    .build()
+            )
+            .build()
+        SingletonImageLoader.setSafe { imageLoader }
+        return imageLoader
+    }
+
+    override fun dependencies(): MutableList<Class<out Initializer<*>>> = Collections.emptyList()
 }
