32ce561b93e9b7e3b924147b8393f1fba5a1cbc3
Akhsaul <ikhsanmaulana384@gmail.com>
2025-05-22 15:05:30 +0700

    Bump versions for several libraries
    This commit updates the following libraries to their newer versions:
    - kotlinx-coroutines-test to 1.10.2
    - Coil to 3.2.0
    - Gson to 2.13.1
    - Datastore Preferences to 1.1.7
    - Retrofit to 3.0.0
    - Annotation to 1.9.1
    - Fragment KTX to 1.8.7

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -71,7 +71,7 @@ dependencies {
     implementation(libs.retrofit)
     implementation(libs.converter.gson)
     implementation(libs.gson)
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.10.1")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.10.2")
     testImplementation("io.insert-koin:koin-test")
     // Koin for JUnit 4
     //testImplementation("io.insert-koin:koin-test-junit4")
