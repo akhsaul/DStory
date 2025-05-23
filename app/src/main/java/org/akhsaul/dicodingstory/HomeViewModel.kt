27bddf492d3771bf89b11cfeada44e80c752fd53
Akhsaul <ikhsanmaulana384@gmail.com>
2025-05-23 13:01:40 +0700

    feat: Add HomeViewModel for handling story creation
    This commit introduces the `HomeViewModel` class, which is responsible for managing the logic related to adding new stories.
    
    Key functionalities:
    - Injects `StoryRepository` for data operations.
    - Manages `currentLocation` as a `MutableStateFlow`.
    - Handles story creation requests through `storyFlow`, a `MutableSharedFlow` of `StoryRequest`.
    - Provides an `addStory` function to emit new story requests to the flow.

---
+++ app/src/main/java/org/akhsaul/dicodingstory/HomeViewModel.kt
@@ -0,0 +1,81 @@
+package org.akhsaul.dicodingstory
+
+import android.location.Location
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.TimeoutCancellationException
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.transformLatest
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withTimeout
+import org.akhsaul.core.data.Result
+import org.akhsaul.core.domain.repository.StoryRepository
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+import java.io.File
+import kotlin.time.Duration.Companion.seconds
+
+class HomeViewModel : ViewModel(), KoinComponent {
+    private val storyRepository: StoryRepository by inject()
+    val currentLocation = MutableStateFlow<Location?>(null)
+    private val storyFlow = MutableSharedFlow<StoryRequest>()
+
+    @OptIn(ExperimentalCoroutinesApi::class, FlowPreview::class)
+    val addStoryResult: Flow<Result<String>> = storyFlow.map {
+        if (it.photo.exists() && it.description.isNotBlank()) {
+            it
+        } else {
+            null
+        }
+    }.filterNotNull().transformLatest { story ->
+        // make ui show progress bar
+        emit(Result.Loading)
+        // wait 30 seconds, if it's still running
+        // then throw TimeoutCancellationException
+        val location = withTimeout(30.seconds) {
+            // always collecting flow
+            // stop when it's value is not null
+            currentLocation.filterNotNull().first()
+        }
+        storyRepository.addStory(
+            story.photo,
+            story.description,
+            location.latitude,
+            location.longitude
+        ).collect {
+            // Don't emit Result.Loading again
+            if (it is Result.Success || it is Result.Error) {
+                emit(it)
+            }
+        }
+    }.catch {
+        if (it is TimeoutCancellationException) {
+            emit(Result.Error("Can't get location!"))
+        } else {
+            emit(Result.Error(it.message))
+        }
+    }
+
+    fun addStory(story: StoryRequest) {
+        viewModelScope.launch {
+            storyFlow.emit(story)
+        }
+    }
+
+    companion object {
+        private const val TAG = "HomeViewModel"
+    }
+}
+
+data class StoryRequest(
+    val photo: File,
+    val description: String,
+)
